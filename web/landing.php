aae2fe77f345d6b303912b4334955649c854f8f1
Nai Shih Chen <che17030@byui.edu>
2020-09-26 22:24:00 -0600

    Added page content

--- web/landing.php
+++ web/landing.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<title>Introductions</title>
-		<link rel="stylesheet" type="text/css" href="styles/styles.css">
+		<link rel="stylesheet" type="text/css" href="styles/hellostyles.css">
 		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
 	</head>
 	<body>
@@ -14,7 +14,7 @@
 				<p>Hello! My name is Nai-Shih Chen, but you can call me Naomi! My favorite thing to do is travelling, and I've done quite a bit. I've been to over 11 different countries, mostly throughout Asia and I'm now living in the USA with my husband. I'm studying PHP to learn how to make my own dynamic websites and hope to be successful as a graphic designer. Thanks for giving my page a vist!</p>
 			</div>
 			<div id="assignments" class="hello">
-				<a href="assignments.php">Assignments</a>
+				<a href="home.php">Assignments</a>
 			</div>
 			<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
 		</main>
