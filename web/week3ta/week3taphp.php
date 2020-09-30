5965b236b34c5ef72503b0b481a9ec185ca76fac
Nai Shih Chen <che17030@byui.edu>
2020-09-29 19:41:26 -0600

    Week 3 Team Activity

---
+++ web/week3ta/week3taphp.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+	</head>
+	<body>
+		Name: <?php echo $_POST["name"]; ?>
+		<br>
+		<br>
+		Email: <a href="<?php echo $_POST["email"]; ?>"><?php echo $_POST["email"]; ?></a>
+		<br>
+		<br>
+		Major: <?php echo $_POST["Major"]; ?>
+		<br>
+		<br>
+		Comments: <?php echo $_POST["Comments"]; ?>
+	</body>
+</html>
