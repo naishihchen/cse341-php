a42c6fa6ac35a3b12e62e62a29a10479e446cd3b
Nai Shih Chen <che17030@byui.edu>
2020-10-03 08:35:12 -0600

    Actually added assignments page

---
+++ web/assignments.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Assignments</title>
+		<link rel="stylesheet" type="text/css" href="styles/hellostyles.css">
+		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
+	</head>
+	<body>
+		<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'; ?>
+		<main>
+			<h2>Coming Soon!</h2>
+			<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
+		</main>
+	</body>
+</html>
