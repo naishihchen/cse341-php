686451636a5acd786ccdcfb0e3f0850daa8f83ce
Nai Shih Chen <che17030@byui.edu>
2020-10-03 08:31:50 -0600

    Reordered pages

--- web/index.php
+++ web/index.php
@@ -1,5 +1,22 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Introductions</title>
+		<link rel="stylesheet" type="text/css" href="styles/hellostyles.css">
+		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
+	</head>
+	<body>
+		<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'; ?>
+		<main>
+			<h2>About Myself </h2>
+			<div id="intro" class="hello">
+				<img src="Naomi.jpg" alt="picture-of-me-and-my-husband">
+				<p>Hello! My name is Nai-Shih Chen, but you can call me Naomi! My favorite thing to do is travelling, and I've done quite a bit. I've been to over 11 different countries, mostly throughout Asia and I'm now living in the USA with my husband. I'm studying PHP to learn how to make my own dynamic websites and hope to be successful as a graphic designer. Thanks for giving my page a vist!</p>
+			</div>
+			<div id="assignments" class="hello">
+				<a href="home.php">Assignments</a>
+			</div>
+			<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
+		</main>
+	</body>
+</html>
