5f53addbc8e66a3dc9987565c2bc3b719f8a9ed0
Nai Shih Chen <che17030@byui.edu>
2020-10-03 20:18:07 -0600

    Fixed CSS and added template

--- web/week3act/items.php
+++ web/week3act/items.php
@@ -12,7 +12,7 @@
 <html lang="en">
 	<head>
 		<title>Soap Store</title>
-		<link rel="stylesheet" type="text/css" href="styles/storeStyles.css">
+		<link rel="stylesheet" type="text/css" href="/styles/storeStyles.css">
 		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
 	</head>
 	<body>
