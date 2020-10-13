dcc93bcfc9e6d44634ae263fd2a493971e5518ca
Nai Shih Chen <che17030@byui.edu>
2020-10-13 10:36:54 -0600

    Added Week 4 team activity

--- web/week4ta/week4ta.php
+++ web/week4ta/week4ta.php
@@ -1,3 +1,35 @@
+<?php
+
+	try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+$statement = $db->query('SELECT * FROM scriptures');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$queryResults = "<p id='scripture-results'>";
+$queryResults .= $results;
+$queryResults .= "</p>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
