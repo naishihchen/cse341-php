da2bb2b789a2cc691aaeba493363eb04f9b4d807
Nai Shih Chen <che17030@byui.edu>
2020-10-17 17:40:10 -0600

    Added purchase search page

--- web/week5act/purchaseSearch.php
+++ web/week5act/purchaseSearch.php
@@ -2,12 +2,12 @@
 session_start();
 include 'database_connect.php';
 
-$product = htmlspecialchars($_GET["product"]);
+//$product = htmlspecialchars($_GET["product"]);
 ​
 if (isset($product)) {
     foreach ($db->query("SELECT purchaseTime, quantity, purchasePrice FROM purchases pu JOIN products pr ON pu.productId = pr.productId WHERE productName = '" . $product . "' AND userid = '" . $_SESSION['userId'] . "';") as $row)
     {
-        echo "<p><strong>";
+        echo "<p>";
         echo $row['purchaseTime'] . " " . $row['quantity'] .":" . $row['purchasePrice'];
         echo '"</p>';
     }
