8de50c10d951649cfab773c0084a641e00e6b8ba
Nai Shih Chen <che17030@byui.edu>
2020-10-17 17:44:59 -0600

    Added purchase search page

--- web/week5act/purchaseSearch.php
+++ web/week5act/purchaseSearch.php
@@ -16,15 +16,22 @@ include 'database_connect.php';
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Receipt Search</title>
-</head>
-<body>
-<h1>Receipt Search
-</h1>
-<p>Please enter the name of the product you wish to search for, with each first letter capitalized.</p>
-<form action="purchaseSearch.php">
-    <input type="text" name="product">
-    <input type="submit">
-</form>
+    <head>
+        <meta charset="UTF-8">
+        <title>Soap Store</title>
+        <link rel="stylesheet" type="text/css" href="/styles/storeStyles.css">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
+    </head>
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/storeHeader.php'; ?>
+        <main>
+            <h1>Receipt Search</h1>
+            <p>Please enter the name of the product you wish to search for, with each first letter capitalized.</p>
+            <form action="purchaseSearch.php">
+                <input type="text" name="product">
+                <input type="submit">
+            </form>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/storeFooter.php'; ?>
+        </main>
+    </body>
+</html>
