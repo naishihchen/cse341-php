ea3441aa9aead4a1ca33d86900652a399495dc79
Nai Shih Chen <che17030@byui.edu>
2020-10-17 15:45:31 -0600

    Added account info page

--- web/week5act/accountInfo.php
+++ web/week5act/accountInfo.php
@@ -10,7 +10,13 @@
 
 	$row = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-	var_dump($row);
+	$queryResults = "<div id='account-info'>";
+
+  	$queryResults .= "<p>Full Name: " . $row[0]['fullname'] . "</p><br>";
+  	$queryResults .= "<p>User Name: " . $row[0]['username'] . "</p><br>";
+	$queryResults .= "<p>Email: " . $row[0]['email'] . "</p>";
+
+	$queryResults .= "</div>";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,6 +29,9 @@
 		<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/storeHeader.php'; ?>
 		<main>
 			<h2>Account Info</h2>
+			<?php if (isset($queryResults)) {
+				echo $queryResults;
+			} ?>
 			<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/storeFooter.php'; ?>
 		</main>
 	</body>
