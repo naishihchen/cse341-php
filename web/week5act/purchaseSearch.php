c153fb2322dd8c367afa8f20656d530a0b4aa776
Nai Shih Chen <che17030@byui.edu>
2020-10-17 18:12:22 -0600

    Added purchase search page

--- web/week5act/purchaseSearch.php
+++ web/week5act/purchaseSearch.php
@@ -28,7 +28,7 @@ $product = htmlspecialchars($_GET["product"]);
                     if (isset($product)) {
                         foreach ($db->query("SELECT purchaseTime, quantity, purchasePrice FROM purchases pu JOIN products pr ON pu.productId = pr.productId WHERE productName = '" . $product . "' AND userid = '" . $_SESSION['userId'] . "';") as $row)
                         {
-                            echo "<div id='purchase-record'>"
+                            echo "<div id='purchase-record'>";
                             echo "<p> Time of Purchase: " . $row['purchasetime'] . "</p><br>";
                             echo "<p>Quantity:  " . $row['quantity'] . "</p><br>";
                             echo "<p>Price: " . $row['purchaseprice'] . "</p>";
