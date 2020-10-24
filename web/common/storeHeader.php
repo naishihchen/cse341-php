840e73060111d28fe78ef0cd5b89122973b87c3a
Nai Shih Chen <che17030@byui.edu>
2020-10-24 14:34:04 -0600

    Added login and registration

--- web/common/storeHeader.php
+++ web/common/storeHeader.php
@@ -1,5 +1,5 @@
 <header>
-	<a href="items.php" id="items-link"><h1>Naomi's Store</h1></a>
+	<a href="web/week6act/items.php" id="items-link"><h1>Naomi's Store</h1></a>
 	<div id="cart">
 		<a href="cart.php">Cart</a>
 		<?php
