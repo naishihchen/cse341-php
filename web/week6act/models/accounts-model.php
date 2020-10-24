6fa6ba756f5894db96ed4bda0d3ec00787e91439
Nai Shih Chen <che17030@byui.edu>
2020-10-24 14:54:05 -0600

    Added login and registration

--- web/week6act/models/accounts-model.php
+++ web/week6act/models/accounts-model.php
@@ -1,7 +1,27 @@
 <?php
 //Accounts Model
 
-require_once '../database_connect.php';
+  try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 
 //Site Registration Function
 function regClient($clientFirstname, $clientLastname, $clientEmail, $clientPassword) {
