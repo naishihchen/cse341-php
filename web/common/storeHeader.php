77f0fe06d24746c8ba787db6383f31cf16770e5b
Nai Shih Chen <che17030@byui.edu>
2020-10-24 22:37:46 -0600

    Added purchase inserts

--- web/common/storeHeader.php
+++ web/common/storeHeader.php
@@ -1,7 +1,9 @@
 <header>
 	<a href="items.php" id="items-link"><h1>Naomi's Store</h1></a>
 	<div id="cart">
-		<a href=".cart.php">Cart</a>
+		<a href="cart.php">Cart</a>
+		</div>
+	<div id="login-registration">
 		<?php
 			if ($_SESSION['loggedin'] == TRUE) {
 				echo "<h1><a href=\"accounts/index.php?action=logout\" id=\"logout\">Log Out</a></h1>";
