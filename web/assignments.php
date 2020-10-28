c6963922538375fbac7f719f8f3a232c625401c6
Nai Shih Chen <che17030@byui.edu>
2020-10-27 19:49:24 -0600

    Added week 7 team activity

--- web/assignments.php
+++ web/assignments.php
@@ -16,6 +16,7 @@
 			<a href="week5act/items.php">Week 5 Assignment</a>
 			<a href="week6ta/week6ta.php">Week 6 Team Activity</a>
 			<a href="week6act/items.php">Week 6 Team Activity</a>
+			<a href="week7ta/home.php">Week 7 Team Activity</a>
 			<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'; ?>
 		</main>
 	</body>
---
+++ web/week7ta/registration.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Soap Store</title>
+		<link rel="stylesheet" type="text/css" href="/styles/storeStyles.css">
+		<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet"> 
+	</head>
+	<body>
+		<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/storeHeader.php'; ?>
+		<main>
+			<h1>Registration</h1>
+
+            <div id="form">
+
+            <?php
+          
+            if (isset($_SESSION['message'])) {
+                echo $_SESSION['message'];
+            }
+            ?>
+
+            <form action="accounts/index.php" method="post">
+                    <label for="fullname">Full Name: </label><input type="text" name="clientFullname" id="fname" required <?php if(isset($clientFullname)){echo "value='$clientFullname'";}  ?>><br>
+                    <label for="username">User Name: </label><input type="text" name="clientUsername" id="uname" required <?php if(isset($clientUsername)){echo "value='$clientUsername'";}  ?>><br>
+                    <label for="email">Email address: </label><input type="email" name="clientEmail" id="email" required <?php if(isset($clientEmail)){echo "value='$clientEmail'";}  ?>><br>
+                    <span>Passwords must be at least 8 characters and contain at least 1 number, 1 capital letter and 1 special character</span> 
+                    <label for="password">Password: </label>
+                    <input type="password" name="clientPassword" id="password" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"><br>
+                <input type="submit" name="submit" id="regbtn" value="Register">
+
+                <input type="hidden" name="action" value="registration">
+            </form>
+            </div>
+			<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/storeFooter.php'; ?>
+		</main>
+	</body>
+</html>
