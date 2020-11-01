41af73bbcf750a7416a464d74652fe1b7ae97a20
Nai Shih Chen <che17030@byui.edu>
2020-10-31 20:39:32 -0600

    Added project 1

--- web/common/storeHeader.php
+++ web/common/storeHeader.php
@@ -1,8 +1,5 @@
 <header>
 	<a href="items.php" id="items-link"><h1>Naomi's Store</h1></a>
-	<div id="cart">
-		<a href="cart.php">Cart</a>
-		</div>
 	<div id="login-registration">
 		<?php
 			if ($_SESSION['loggedin'] == TRUE) {
