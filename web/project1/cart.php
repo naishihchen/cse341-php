8504614169bd1c69e8b66a424fd2b0d002e9b90d
Nai Shih Chen <che17030@byui.edu>
2020-10-31 19:59:18 -0600

    Added project 1

--- web/project1/cart.php
+++ web/project1/cart.php
@@ -6,9 +6,9 @@
   $action = filter_input(INPUT_GET, 'action');
  }
 
-	$lavQuantity = filter_input(INPUT_POST, 'Lavender Soap Quantity', FILTER_SANITIZE_NUMBER_INT);
-	$mintQuantity = filter_input(INPUT_POST, 'Mint Soap Quantity', FILTER_SANITIZE_NUMBER_INT);
-	$oatQuantity = filter_input(INPUT_POST, 'Oatmeal Soap Quantity', FILTER_SANITIZE_NUMBER_INT);
+	$lavQuantity = filter_input(INPUT_POST, 'Lavender Soap Quantity', FILTER_SANITIZE_NUMBER_FLOAT);
+	$mintQuantity = filter_input(INPUT_POST, 'Mint Soap Quantity', FILTER_SANITIZE_NUMBER_FLOAT);
+	$oatQuantity = filter_input(INPUT_POST, 'Oatmeal Soap Quantity', FILTER_SANITIZE_NUMBER_FLOAT);
 
 	if ($lavQuantity > 0) {
 		$_SESSION['cart'][0]['productName'] = 'Lavender Soap';
@@ -32,6 +32,8 @@
 	}
 
 	var_dump($lavQuantity);
+	var_dump($mintQuantity);
+	var_dump($mintQuantity);
 
 	var_dump($_SESSION['cart']);
 
